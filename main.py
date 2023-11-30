08ff2830b6b271119ffda706177197e6742af40e
Basil Sutormin <sudobasil@gmail.com>
2023-11-30 12:03:10 +0100

    Entry + Button + db

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import psycopg2
 import os
-import json
-from data_tmp import verb
-
-def everything_is_ok(name):
-    conn = psycopg2.connect(host='localhost', dbname=os.environ["SQL_db_name"], user='postgres',
-                            password=os.environ["SQL_password"], port=5433)
-    cur = conn.cursor()
-    cur.execute("""SELECT COUNT(*) FROM customer""")
-    print(cur.fetchone())
-
-    conn.commit()
-    cur.close()
-    conn.close()
-    print(f'THIS IS {name}')
+import tkinter as tk
 
 
+class SentenceGenerator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Generate Sentences")
 
-if __name__ == '__main__':
+        self.entry_verb = tk.Entry(self.root)
+        self.entry_verb.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-   everything_is_ok('OK')
+        self.btn_check = tk.Button(self.root, text="Check", command=self.check_verb)
+        self.btn_check.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
+    def check_verb(self):
+        verb = self.entry_verb.get()
+        conn = psycopg2.connect(host='localhost',
+                                user='postgres',
+                                database=os.environ["SQL_db_name"],
+                                password=os.environ["SQL_password"],
+                                port=5433)
+        cur = conn.cursor()
+        query = f"SELECT * FROM verb_a WHERE inf_eng = '{verb}'"
+        cur.execute(query)
+        print(cur.fetchone())
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    root_ = tk.Tk()
+    application = SentenceGenerator(root_)
+    root_.mainloop()
 
